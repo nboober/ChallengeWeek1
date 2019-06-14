ed41c039695676dcc1a4aab742c06b5d2ac058df
nboober <nboober@terpmail.umd.edu>
2019-06-14 11:35:42 -0400

    Created a loop on the program so it only exits when the user types quit at certain points

--- src/ChallengeWeek1.java
+++ src/ChallengeWeek1.java
@@ -6,80 +6,89 @@ public class ChallengeWeek1 {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
 
+        String option = "";
         int myNum;
         String conf = "";
         String cont = "";
-        String option = "";
         int range;
 
-        System.out.println("Would you like to generate a random number? (yes/no)");
-        option = scanner.next();
+        do{
 
-        if(option.equalsIgnoreCase("no")){
+            System.out.println("Would you like to generate a random number? If not you will be prompted to enter your own number (yes/no). \nTo quit the program type \"quit\".");
+            option = scanner.next();
 
-            do{
+            if(option.equalsIgnoreCase("no")){
 
-                System.out.println("Enter a non-decimal number. I will let you know if it is a prime number :) : ");
-                myNum = scanner.nextInt();
+                do{
 
-                if(myNum%2 == 0){
-                    conf = "Not";
+                    System.out.println("Enter a non-decimal number. I will let you know if it is a prime number :) : ");
+                    myNum = scanner.nextInt();
 
-                }else{
-                    for(int test = 3; test < myNum; test++){
-                        if(myNum%test == 0){
-                            conf = "Not";
-                            break;
-                        }else{
-                            conf = "Prime";
+                    if(myNum%2 == 0){
+                        conf = "Not";
+
+                    }else{
+                        for(int test = 3; test < myNum; test++){
+                            if(myNum%test == 0){
+                                conf = "Not";
+                                break;
+                            }else{
+                                conf = "Prime";
+                            }
                         }
                     }
-                }
-                if(conf.equals("Prime")){
-                    System.out.println(myNum + " is a Prime Number!");
-                }else{
-                    System.out.println(myNum + " is NOT a Prime Number!");
-                }
+                    if(conf.equals("Prime")){
+                        System.out.println(myNum + " is a Prime Number!");
+                    }else{
+                        System.out.println(myNum + " is NOT a Prime Number!");
+                    }
 
-                System.out.println("Would you like to enter another number? (yes/no)");
-                cont = scanner.next();
+                    System.out.println("Would you like to enter another number? (yes/no). To quit the program type \"quit\".");
+                    cont = scanner.next();
 
-            }while(cont.equalsIgnoreCase("yes"));
+                }while(cont.equalsIgnoreCase("yes"));
 
-        }else{
+            }else if(option.equalsIgnoreCase("yes")){
 
-            do{
+                do{
 
-                System.out.println("What would you like the range to go up to, starting from 1? ");
-                range = scanner.nextInt();
-                System.out.println("Generating a random number...");
-                myNum = 1 + rand.nextInt(range);
+                    System.out.println("What would you like the range to go up to, starting from 1? ");
+                    range = scanner.nextInt();
+                    System.out.println("Generating a random number...");
+                    myNum = 1 + rand.nextInt(range);
 
-                if(myNum%2 == 0){
-                    conf = "Not";
+                    if(myNum%2 == 0){
+                        conf = "Not";
 
-                }else{
-                    for(int test = 3; test < myNum; test++){
-                        if(myNum%test == 0){
-                            conf = "Not";
-                            break;
-                        }else{
-                            conf = "Prime";
+                    }else{
+                        for(int test = 3; test < myNum; test++){
+                            if(myNum%test == 0){
+                                conf = "Not";
+                                break;
+                            }else{
+                                conf = "Prime";
+                            }
                         }
                     }
-                }
-                if(conf.equals("Prime")){
-                    System.out.println(myNum + " is a Prime Number!");
-                }else{
-                    System.out.println(myNum + " is NOT a Prime Number!");
-                }
+                    if(conf.equals("Prime")){
+                        System.out.println(myNum + " is a Prime Number!");
+                    }else{
+                        System.out.println(myNum + " is NOT a Prime Number!");
+                    }
 
-                System.out.println("Would you like to test another random number? (yes/no)");
-                cont = scanner.next();
+                    System.out.println("Would you like to test another random number? (yes/no). To quit the program type \"quit\".");
+                    cont = scanner.next();
 
-            }while(cont.equalsIgnoreCase("yes"));
+                }while(cont.equalsIgnoreCase("yes"));
 
-        }
+            }else if(option.equalsIgnoreCase("quit")){
+                break;
+            }else{
+                System.out.println("That is not an applicable entry.");
+                cont = "no";
+            }
+
+        }while(!cont.equalsIgnoreCase("yes"));
 
 
     }
