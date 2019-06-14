147c497252431fec7809768d63914f5e63890dc6
nboober <nboober@terpmail.umd.edu>
2019-06-14 10:42:03 -0400

    Created an option sequence at the beginning of the app to choose between two different number generating methods

--- src/ChallengeWeek1.java
+++ src/ChallengeWeek1.java
@@ -5,30 +5,49 @@ public class ChallengeWeek1 {
         Scanner scanner = new Scanner(System.in);
         int myNum;
         String conf = "";
+        String cont = "";
+        String option = "";
 
+        System.out.println("Would you like to generate a random number? (yes/no)");
+        option = scanner.next();
 
+        if(option.equalsIgnoreCase("no")){
 
-        System.out.println("Enter a non-decimal number. I will let you know if it is a prime number :) : ");
-        myNum = scanner.nextInt();
+            do{
 
-        if(myNum%2 == 0){
-            conf = "Not";
+                System.out.println("Enter a non-decimal number. I will let you know if it is a prime number :) : ");
+                myNum = scanner.nextInt();
 
-        }else{
-            for(int test = 3; test < myNum; test++){
-                if(myNum%test == 0){
+                if(myNum%2 == 0){
                     conf = "Not";
-                    break;
+
                 }else{
-                    conf = "Prime";
+                    for(int test = 3; test < myNum; test++){
+                        if(myNum%test == 0){
+                            conf = "Not";
+                            break;
+                        }else{
+                            conf = "Prime";
+                        }
+                    }
                 }
-            }
-        }
-        if(conf.equals("Prime")){
-            System.out.println(myNum + " is a Prime Number!");
+                if(conf.equals("Prime")){
+                    System.out.println(myNum + " is a Prime Number!");
+                }else{
+                    System.out.println(myNum + " is NOT a Prime Number!");
+                }
+
+                System.out.println("Would you like to enter another number? (yes/no)");
+                cont = scanner.next();
+
+            }while(cont.equalsIgnoreCase("yes"));
+
         }else{
-            System.out.println(myNum + " is NOT a Prime Number!");
+
+            System.out.println("Generating a random number...");
+
         }
+
 
     }
 
