9dd8a40f2bd66b31876d0e19d26adca3dd185ae0
nboober <nboober@terpmail.umd.edu>
2019-06-14 10:28:09 -0400

    Created an algorithm to determine if a number entered is prime or not. The result is displayed.

--- src/ChallengeWeek1.java
+++ src/ChallengeWeek1.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
+
 public class ChallengeWeek1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int myNum;
+        String conf = "";
+
+
+
+        System.out.println("Enter a non-decimal number. I will let you know if it is a prime number :) : ");
+        myNum = scanner.nextInt();
+
+        if(myNum%2 == 0){
+            conf = "Not";
+
+        }else{
+            for(int test = 3; test < myNum; test++){
+                if(myNum%test == 0){
+                    conf = "Not";
+                    break;
+                }else{
+                    conf = "Prime";
+                }
+            }
+        }
+        if(conf.equals("Prime")){
+            System.out.println(myNum + " is a Prime Number!");
+        }else{
+            System.out.println(myNum + " is NOT a Prime Number!");
+        }
+
+    }
+
 }
